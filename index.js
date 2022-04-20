aef2ef003a1fb9f28546bc55714e459ed8a574ef
DARSHIT TARAPARA <94553274+Darshittarapara@users.noreply.github.com>
2022-04-20 22:19:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+    let array=[]
+    const getvalue =(b)=>{
+        let gets=document.getElementById("get").innerHTML
+     gets=b
+    }
+    const take=(a)=>{
+        let btn =document.getElementsByClassName("list");
+        let res=btn[a].innerHTML
+    array=res
+    getvalue(array)
+    }
+    // let result=take()
+    // console.log(result)
+  
